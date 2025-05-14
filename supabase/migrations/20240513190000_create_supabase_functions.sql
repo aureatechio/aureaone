4ec521f0f29ea99147b04bd7bf0601027fac5c81
aureatechio <integracao@aureatech.io>
2025-05-14 15:57:14 -0300

    fix: Atualização da view viewSocialAccount com nomes em português

--- supabase/migrations/20240513190000_create_supabase_functions.sql
+++ supabase/migrations/20240513190000_create_supabase_functions.sql
@@ -912,16 +912,16 @@ FROM "SateliteCamposFormPreviewMidia";
 
 CREATE OR REPLACE VIEW public."viewSocialAccount" AS
 SELECT 
-  plataforma::osPlataformas,
-  creator,
-  "fotoPerfil",
-  "idPagina",
-  "nomePagina",
-  access_token,
-  id,
-  empresa,
-  status::osStatus
-FROM "PaginasAnuncio";
+  p."contaBusinessPaginaId" as "idContaBusiness",
+  p.creator as "criador",
+  p."fotoPerfil" as "fotoPerfil",
+  p."idPagina" as "idPagina",
+  p."nomePagina" as "nomePagina",
+  p."accessToken" as "tokenAcesso",
+  p.id as "id",
+  p.empresa as "empresa",
+  p.status::osStatus as "status"
+FROM "PaginasAnuncio" p;
 
 CREATE OR REPLACE VIEW public."viewSocialAccounts_ads" AS
 SELECT 
